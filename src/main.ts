fa5ab5822e7c61ca6b111ea8fe65b203e102925a
WesleyEstevam <wesleyestevam15@gmail.com>
2023-03-14 15:19:28 -0300

    Aquivo .env no gitinignore

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const porta = 3000;
+  await app.listen(porta, () => {
+    console.log("Servidor rodando na porta: " + porta)
+  });
 }
 bootstrap();
