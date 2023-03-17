c697a955a620d4220f38f1167e811be137e78f18
WesleyEstevam <wesleyestevam15@gmail.com>
2023-03-17 11:31:52 -0300

    Cors adicionado

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   const porta = 3000;
   await app.listen(porta, () => {
     console.log("Servidor rodando na porta: " + porta)
