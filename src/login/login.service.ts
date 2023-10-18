2293652a774bbc117d25709c414649f1751de89b
WesleyEstevam <wesleyestevam15@gmail.com>
2023-10-18 14:33:00 -0300

    Autenticação de porteiro implementada

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -44,6 +44,10 @@ export class LoginService {
       throw new Error('Usuário não encontrado');
     }
 
+    if (user.role === 'Morador') {
+      throw new Error('Usuário não é um porteiro');
+    }
+
     // Compara a senha fornecida com a senha armazenada no usuário
     if (!bcrypt.compareSync(senha, user.senha)) {
       throw new Error('Credenciais inválidas');
